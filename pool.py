693e0b87203443e70b08c8839d668da4f08be3e3
shiyihang2007 <467557146@qq.com>
2023-05-26 19:02:40 +0800

    add pool.pool.findGame

---
+++ pool.py
@@ -0,0 +1,11 @@
+from .game import game
+
+
+class pool:
+    gameList: list[game]
+    
+    def findGame(self, group: str) -> game:
+        for i in self.gameList:
+            if i.group == group:
+                return i
+        raise KeyError
